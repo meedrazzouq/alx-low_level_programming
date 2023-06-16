7e570ae2b57e31c0e76257eff54e5ad5aef3559e
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-15 18:10:16 -0700

    task 5 in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a prgram that prints a line using printf function
+ * Return: 0 if the program compiled with no errors
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
