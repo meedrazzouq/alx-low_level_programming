807e77650d8781b9fe3d3c301ddeccebf6533110
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-15 17:31:21 -0700

    task 4 in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle"); return 0;
+#include<std.h>
+/**
+ * main - a program that prints a line using puts function
+ * return : 0(success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
