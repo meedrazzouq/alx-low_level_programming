3b7eb50d3d26d0974d94d519727d67a15b1e38fe
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-15 18:03:31 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using puts function
- * Return : 0(Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 when there is no errors
  */
 int main(void)
 {
