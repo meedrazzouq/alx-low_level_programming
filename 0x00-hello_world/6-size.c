fbd0adba69925b408b913260b50c9fe72324b0db
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-15 18:57:06 -0700

    task 6 in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a  program that prints the size of various types on the computer
+ * return : 0 if the code got compiled with no error
+ */
+int main(void)
+{
+printf("Size of char:  %zu bytes\n", sizeof(char));
+printf("Size of an int: %zu  byte(s\n)", sizeof(int));
+printf("Size of a long int: %zu  byte(s\n)", sizeof(long int));
+printf("Size of a long long int: %zu  byte(s\n)", sizeof(long long int));
+printf("Size of a float: %zu  byte(s\n)", sizeof(float));
+return (0);
+}
