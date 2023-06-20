d3df1dd71257b0c23c7a4ff0441d56788ea5b4ae
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-20 15:21:07 -0700

    task 0 in c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - the starting function of the program
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
