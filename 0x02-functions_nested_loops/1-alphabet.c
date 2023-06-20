a5765b1bca019206c7fd14836122d22da2f75797
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-20 16:26:47 -0700

    task 1 in c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * function - prints the alphabets in lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+     char c ;
+     for(c = 'a';c <= 'z';c++)
+     {
+	    _putchar(c);
+     }
+     _putchar('\n');
+}
