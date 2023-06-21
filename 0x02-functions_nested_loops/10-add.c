87e32d696398f84b7e0117fcedcb9dc40e4c3c40
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-21 09:31:40 -0700

    task 10 in c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -  a function that adds two integers and returns the result
+ * @n1: the parameter that used for the addition operation
+ * @n2: the parameter that used for the addition operation
+ * Return: sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ * Retrun: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d/n", n);
+	return (0);
+}
