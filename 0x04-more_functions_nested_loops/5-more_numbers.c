2e35e0210d5077d3f501b84d12db6da912ec3993
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-22 17:29:34 -0700

    task in nested loops and functions in c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 15)
+	{
+		int j;
+
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('0' + i);
+		}
+		_putcahr('\n');
+		i++
+	}
+}
