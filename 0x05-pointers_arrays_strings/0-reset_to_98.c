b4cf4e1553172e5dddb80cf6c970ca32a86b12c9
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-06-26 18:59:09 -0700

    task 0 in c pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - update a value of a variable using pointer
+ * @n: pointer that will be passed to change the variable value
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
