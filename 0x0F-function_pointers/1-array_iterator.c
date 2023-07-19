8960e9a95a9071c9ea052eb391d6465fc2079f71
Mohamed Razzouq <mohamedrazzouq3110@gmail.com>
2023-07-19 09:03:46 -0700

    if you spend too much time thinking of a thing , you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,15 +7,16 @@
  * @action: pointer to print in regular or hex
  * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
-		action(arry[i]);
+		action(array[i]);
 	}
 }
+
